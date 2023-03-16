99520280cfeaf082c11025c5a84b70bd90e8818e
ofont <o.font@alumnos.upm.es>
2023-03-16 20:18:42 +0100

    setup of the project

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta
+          name="description"
+          content="AI Virtual Assitant chat web app for The Petit Palace Hotel clients"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className="text-3xl font-bold italic">Hello world!</h1>
+      </main>
+    </>
+  )
+}
