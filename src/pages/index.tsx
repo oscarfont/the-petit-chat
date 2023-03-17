0bb2bcc7eb220b21716c51d07781bc7705d3bbeb
ofont <o.font@alumnos.upm.es>
2023-03-17 08:38:05 +0100

    added sample tests for backend code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
@@ -17,7 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main>
         <h1 className="text-3xl font-bold italic">Hello world!</h1>
       </main>
     </>
---
+++ src/test/unit/dto.test.ts
@@ -0,0 +1,6 @@
+describe("sample test", () => {
+  test("should return true", () => {
+    const value = true;
+    expect(value).toBe(true);
+  });
+});
