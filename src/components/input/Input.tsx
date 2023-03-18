f5f7767f9742af8ad3d3690692889bd974dd83de
ofont <o.font@alumnos.upm.es>
2023-03-18 23:38:32 +0100

    added send icon and form

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,7 +1,14 @@
+import { SendIcon } from "../Icons";
+
 export default function Input() {
     return (
-      <>
-        <input className="w-full p-2 rounded-2xl" placeholder="Escribe aquí tu mensaje..."/>
-      </>
+        <form>
+            <div className="relative block">
+                <input className="w-full p-2 rounded-2xl" placeholder="Escribe aquí tu mensaje..." type="text"/>
+                <button type="submit">
+                    <SendIcon />
+                </button>
+            </div>
+        </form>
     )
   }
