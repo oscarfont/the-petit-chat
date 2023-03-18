d76d70eb4a5f10b3d2f3663216d6be25d7079968
ofont <o.font@alumnos.upm.es>
2023-03-18 17:52:14 +0100

    added input box of chat

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,10 +1,14 @@
 import Header from "../header/Header";
+import Input from "../input/Input";
 
 export default function Chat() {
     return (
-    <div className="w-full max-w-lg h-screen flex flex-col px-2 border-solid border-2 border-red-800">
+    <section className="w-full max-w-lg h-screen flex flex-col px-2 border-solid border-2 border-red-800">
         <Header />
-        CHAT SECTION
-    </div>
+        <div className="w-full h-full border-solid border-2 border-red-800">
+            Messages Placeholder
+        </div>
+        <Input />
+    </section>
     )
 }
---
+++ src/components/input/Input.tsx
@@ -0,0 +1,7 @@
+export default function Input() {
+    return (
+      <>
+        <input className="w-full p-2 rounded-2xl" placeholder="Escribe aquí tu mensaje..."/>
+      </>
+    )
+  }
